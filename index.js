90887553c87d353689a284475d4d5dc9e3fb5806
shiny1912 <shiny1912@naver.com>
2018-12-23 17:35:19 +0900

    Your First express server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hi");
+const express = require('express'); //express 모듈을 폴더에서 찾는 코드
+const app = express();
+
+const PORT = 4000;
+
+function handleListening (){
+console.log('Listening on: http://localhost:4000');
+
+}
+
+app.listen(PORT, handleListening);
